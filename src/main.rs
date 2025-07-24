295f13e3d12186f14f6ce2e9f9c27c93f49e0218
nhhvhy <maxcostler@gmail.com>
2025-07-23 19:55:22 -0600

    inital collision implementation

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,16 @@
-use bevy::prelude::*;
+#[warn(unused_imports)]
+use bevy::{
+    prelude::*,
+    math::bounding::{Aabb2d, BoundingCircle, BoundingVolume, IntersectsVolume}
+};
+
 use bevy::sprite::{Wireframe2dConfig, Wireframe2dPlugin};
+
+const CUBE_ICON_WIDTH: f32 = 50.0;
+const CUBE_ICON_HEIGHT: f32 = 50.0;
+
+#[derive(Component, Deref, DerefMut)]
+struct Velocity(Vec2);
 
 #[derive(Component)]
 struct Icon;
@@ -11,6 +22,15 @@ struct JumpTimer {
     steps_remaining: i32
 }
 
+#[derive(Event, Default)]
+struct CollisionEvent;
+
+#[derive(Component, Default)]
+struct Collider;
+
+#[derive(Component)]
+#[require(Collider)]
+struct Ground;
 
 fn main() {
     App::new()
@@ -20,7 +40,8 @@ fn main() {
         ))
         .add_systems(Startup, setup)
         .add_systems(Update, toggle_wireframe)
-        .add_systems(Update, (icon_on_click, cube_jump))
+        .add_event::<CollisionEvent>()
+        .add_systems(Update, (icon_on_click, cube_jump, collision_check))
         .run();
 }
 
@@ -37,18 +58,22 @@ fn toggle_wireframe(
 fn icon_on_click(
     mouse_input: Res<ButtonInput<MouseButton>>,
     mut commands: Commands,
-    query: Query<Entity, With<Icon>>
+    query: Query<(Entity, Option<&JumpTimer>), With<Icon>>
 ) {
     if mouse_input.just_pressed(MouseButton::Left) {
         println!("LMB clicked");
 
-        if let Ok(icon_entity) = query.single() {
-            commands.entity(icon_entity).insert(
-                JumpTimer {
-                timer: Timer::from_seconds(0.01, TimerMode::Repeating),
-                dir: 1.0,
-                steps_remaining: 12
-            });
+        if let Ok((icon_entity, jump_timer )) = query.single() {
+            if jump_timer.is_some() {
+                return
+            } else {
+                commands.entity(icon_entity).insert(
+                    JumpTimer {
+                    timer: Timer::from_seconds(0.01, TimerMode::Repeating),
+                    dir: 1.0,
+                    steps_remaining: 12
+                });
+            }
         }
     }
 }
@@ -72,10 +97,47 @@ fn cube_jump(
                 }
             }
         }
-    } 
+    }
 }
 
+fn collision_check (
+    icon_query: Single<&Transform, With<Icon>>,
+    collider_query: Query<(Entity, &Transform), With<Collider>>,
+    mut collision_events: EventWriter<CollisionEvent>
+) {
+    let icon_transform = icon_query.into_inner();
 
+    for (_collider_entity, collider_transform) in &collider_query {
+        let collision = cube_collision(
+            Aabb2d::new(
+                icon_transform.translation.truncate(),
+                icon_transform.scale.truncate() / 2.
+            ),
+            Aabb2d::new(
+                collider_transform.translation.truncate(),
+                collider_transform.scale.truncate() / 2.
+            )
+        );
+
+        if collision {
+             collision_events.write_default();
+        }
+    }
+}
+
+fn cube_collision (
+    cube: Aabb2d,
+    bounding_box: Aabb2d
+) -> bool {
+
+    if !cube.intersects(&bounding_box) {
+        return false;
+    } else {
+        return true;
+    }
+
+    
+}
 fn setup(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
@@ -83,11 +145,12 @@ fn setup(
 ) {
     commands.spawn(Camera2d);
 
-    let cube = meshes.add(Rectangle::new(50.0, 50.0));
+    let cube = meshes.add(Rectangle::new(CUBE_ICON_WIDTH, CUBE_ICON_HEIGHT));
     commands.spawn((
         Mesh2d(cube),
         MeshMaterial2d(materials.add(Color::hsv(0., 1., 1.))),
         Transform::from_xyz(-400.0, -100.0, 0.0),
+        Velocity(Vec2::new(1.0, 0.0)),
         Icon
     ));
 
@@ -95,6 +158,7 @@ fn setup(
     commands.spawn((
         Mesh2d(ground),
         MeshMaterial2d(materials.add(Color::hsv(0.3, 0.4, 0.4))),
-        Transform::from_xyz(200.0, -375.0, 0.0)
+        Transform::from_xyz(200.0, -375.0, 0.0),
+        Ground
     ));
 }
